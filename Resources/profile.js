0c748b22937a00e9bb114db83783f143c0521b24
Luiz <otavio.tata.morais@gmail.com>
2024-08-16 19:18:31 -0300

    adicionado a funcao overlay

--- Resources/profile.js
+++ Resources/profile.js
@@ -3,11 +3,20 @@ changePasswordBtn = document.querySelector('#change-password-button');
 
 containerChangeName = document.querySelector('.change-name-container');
 containerChangePassword = document.querySelector('.change-password-container');
+overlay = document.querySelector('.overlay');
 
 changeNameBtn.addEventListener('click', () => {
     containerChangeName.style.display = "flex";
+    overlay.style.display = "block";
 });
 
 changePasswordBtn.addEventListener('click', () => {
     containerChangePassword.style.display= "flex";
+    overlay.style.display = "block";
 })
+
+overlay.onclick = () => {
+    containerChangeName.style.display = "none";
+    containerChangePassword.style.display = "none";
+    overlay.style.display = "none";
+}

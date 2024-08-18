a1aeca11b8022ff1979f9239640f19e4744c47e8
Luiz <otavio.tata.morais@gmail.com>
2024-08-18 19:48:45 -0300

    incluindo o arquivo showTransactions

--- View/home.php
+++ View/home.php
@@ -24,8 +24,7 @@ if(!isset($_SESSION["user"])){
 ?>
 
 <?php 
-    $sql = "SELECT nome_transacao, valor, data, tipo_transacao FROM transacoes ORDER BY data DESC";
-    $result = $conn->query($sql);
+    
 ?>
 
 <!DOCTYPE html>
@@ -60,34 +59,7 @@ if(!isset($_SESSION["user"])){
         </div>
         <div class="transactions">
             <span>Histórico de Transações</span>
-            <?php
-                if($result->num_rows > 0){
-                    while($row = $result->Fetch_assoc()){
-                        ?>
-                        <div class="transactions-item">
-                            <div>
-                                <span><?php echo htmlspecialchars($row['nome_transacao']); ?></span>
-                                <span><?php echo htmlspecialchars(date("F j, Y", strtotime($row['data']))); ?></span>
-                            </div>
-                            <div>
-                                <span><?php echo number_format($row['valor'], 2); ?></span>
-                                <span>
-                                    <?php 
-                                        if($row['tipo_transacao'] === 1){
-                                            echo htmlspecialchars("Renda");
-                                        }else{
-                                            echo htmlspecialchars("Despesa");
-                                        }
-                                    ?>
-                                </span>
-                            </div>
-                        </div>
-                        <?php
-                    }
-                }else{
-                    echo ("<span>Você não tem transações, adicione!</span>");
-                }
-            ?>
+            <?php include '../resources/showTransactions.php'; ?>
         </div>
         <footer class="navigation-bar">
             <div class="navigation-bar-items">

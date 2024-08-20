348db82d8644e3eac29876d19c432aa8a7b5d90e
Luiz <otavio.tata.morais@gmail.com>
2024-08-20 08:55:25 -0300

    exportando para um arquivo externo, para melhor a integracao em outras telas.

--- View/home.php
+++ View/home.php
@@ -24,19 +24,7 @@ if(!isset($_SESSION["user"])){
 ?>
 
 <?php
-    // Consulta para somar todas as rendas
-    $sql_renda = "SELECT SUM(valor) AS total_renda FROM transacoes WHERE tipo_transacao = 1";
-    $result_renda = $conn->query($sql_renda);
-    $row_renda = $result_renda->fetch_assoc();
-    $total_renda = (float) ($row_renda['total_renda'] ?? 0);
-
-    // Consulta para somar todas as despesas
-    $sql_despesa = "SELECT SUM(valor) AS total_despesa FROM transacoes WHERE tipo_transacao = 0";
-    $result_despesa = $conn->query($sql_despesa);
-    $row_despesa = $result_despesa->fetch_assoc();
-    $total_despesa = (float) ($row_despesa['total_despesa'] ?? 0);
-
-    $saldo_total = $total_renda - $total_despesa;
+    include "../resources/balanceRendaDespesa.php";
 ?>
 
 <!DOCTYPE html>

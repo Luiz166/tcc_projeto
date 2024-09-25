4732a8aca251af6d7b68a83d84375d958cff3df6
Luiz <otavio.tata.morais@gmail.com>
2024-09-25 17:34:06 -0300

    limpando código

--- Resources/SearchBar.js
+++ Resources/SearchBar.js
@@ -4,17 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (typeof searchQuery === 'string' && searchQuery.trim() !== "") {
             searchQuery = searchQuery.toLowerCase();
-
-            // Envio da requisição AJAX
-            // var xhr = new XMLHttpRequest();
-            // xhr.open('GET', '../Resources/search.php?search=' + encodeURIComponent(searchQuery), true);
-            // xhr.onreadystatechange = function() {
-            //     if (xhr.readyState === 4 && xhr.status === 200) {
-            //         document.getElementsByClassName('transactions').innerHTML = xhr.responseText;
-            //     }
-            // };
-            // xhr.send();
-
             fetch('../Resources/search.php?search=' + encodeURIComponent(searchQuery))
             .then(res => res.text())
             .then(body => {

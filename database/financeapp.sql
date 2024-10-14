01880c5eafc694d8f5bcfd46eb4e127c37da854f
Luiz166 <otavio.tata.morais@gmail.com>
2024-10-14 16:03:01 -0300

    updated database

--- database/financeapp.sql
+++ database/financeapp.sql
@@ -1,34 +1,56 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 17/09/2024 às 16:01
--- Versão do servidor: 8.3.0
--- Versão do PHP: 8.2.18
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: financeapp
+-- ------------------------------------------------------
+-- Server version	8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `financeapp`
+-- Table structure for table `metas`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `metas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `metas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `valor_meta` decimal(10,2) NOT NULL,
+  `gasto_total` decimal(10,2) DEFAULT '0.00',
+  `rendimento` decimal(10,2) GENERATED ALWAYS AS ((`valor_meta` - `gasto_total`)) STORED,
+  `mes` int NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para tabela `transacoes`
+-- Dumping data for table `metas`
+--
+
+LOCK TABLES `metas` WRITE;
+/*!40000 ALTER TABLE `metas` DISABLE KEYS */;
+INSERT INTO `metas` (`id`, `valor_meta`, `gasto_total`, `mes`, `nome`) VALUES (1,1000.00,200.00,11,'meta nov');
+/*!40000 ALTER TABLE `metas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transacoes`
 --
 
 DROP TABLE IF EXISTS `transacoes`;
-CREATE TABLE IF NOT EXISTS `transacoes` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transacoes` (
   `transacao_id` int NOT NULL AUTO_INCREMENT,
   `valor` float NOT NULL,
   `nome_transacao` varchar(30) NOT NULL,
@@ -36,44 +58,56 @@ CREATE TABLE IF NOT EXISTS `transacoes` (
   `tipo_transacao` tinyint(1) NOT NULL,
   `categoria` varchar(20) DEFAULT NULL,
   `usuario_id` int DEFAULT NULL,
+  `meta_id` int DEFAULT NULL,
   PRIMARY KEY (`transacao_id`),
-  KEY `fk_usuario_id` (`usuario_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_usuario_id` (`usuario_id`),
+  KEY `fk_meta` (`meta_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Despejando dados para a tabela `transacoes`
+-- Dumping data for table `transacoes`
 --
 
-INSERT INTO `transacoes` (`transacao_id`, `valor`, `nome_transacao`, `data`, `tipo_transacao`, `categoria`, `usuario_id`) VALUES
-(15, 1000, 'salario', '2024-08-21', 1, NULL, NULL),
-(8, 100, 'celular', '2024-08-18', 0, NULL, NULL),
-(14, 200, 'teste', '2024-08-19', 0, NULL, NULL),
-(17, 100, 'carne', '2024-09-17', 0, 'mercado', 3);
-
--- --------------------------------------------------------
+LOCK TABLES `transacoes` WRITE;
+/*!40000 ALTER TABLE `transacoes` DISABLE KEYS */;
+INSERT INTO `transacoes` VALUES (15,1000,'salario','2024-08-21',1,NULL,NULL,NULL),(8,100,'celular','2024-08-18',0,NULL,NULL,NULL),(14,200,'teste','2024-08-19',0,NULL,NULL,NULL),(17,100,'carne','2024-09-17',0,'mercado',3,NULL),(18,1200,'salario','2024-10-14',1,'renda',4,0),(19,200,'pedivela','2024-10-14',0,'bike',4,1);
+/*!40000 ALTER TABLE `transacoes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura para tabela `usuario`
+-- Table structure for table `usuario`
 --
 
 DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
   `usuario_id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `senha` varchar(100) NOT NULL,
   PRIMARY KEY (`usuario_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Despejando dados para a tabela `usuario`
+-- Dumping data for table `usuario`
 --
 
-INSERT INTO `usuario` (`usuario_id`, `nome`, `email`, `senha`) VALUES
-(1, 'Mark Smith Jr Jr', 'marksmith@gmail.com', '$2y$10$K3GyrJ33FdZo3pMKREGRb.pQ/nZ9lWYWKqHDhQX52XTTt24UbjWge'),
-(3, 'Mark Smith 2', 'marksmith2@gmail.com', '$2y$10$Ms91P/yzs.dSp1dW5AeBnuVRzKMT31wKs5ynNRCDUc/Zegyedcf6W');
-COMMIT;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Mark Smith Jr Jr','marksmith@gmail.com','$2y$10$K3GyrJ33FdZo3pMKREGRb.pQ/nZ9lWYWKqHDhQX52XTTt24UbjWge'),(3,'Mark Smith 2','marksmith2@gmail.com','$2y$10$Ms91P/yzs.dSp1dW5AeBnuVRzKMT31wKs5ynNRCDUc/Zegyedcf6W'),(4,'teste','teste@gmail.com','$2y$10$JBXEhyxScRPmeOJ7I4AQluAZF2f38jlQgWZScIjWeuKQ4JUZ9/CAG');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-14 16:00:28
